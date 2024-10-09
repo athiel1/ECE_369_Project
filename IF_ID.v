9e7157528109a0ac3c0d3769429bdb1b0687d01b
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-09 15:22:30 -0700

    Update IF_ID.v

--- IF_ID.v
+++ IF_ID.v
@@ -1,1 +1,27 @@
+`timescale 1ns / 1ps
+
+
+module IF_ID(PC_in, instructionMem_in, PC_out, instructionMem_out); 
+  
+  input [31:0] PC_in;
+  input [31:0] instructionMem_in;
+
+  output [31:0] PC_out;
+  output [31:0] instructionMem_out;
+
+  always @(*) begin
+    PC_out <= PC_in;
+    instructionMem_out <= instructionMem_in;
+  end
+
+
+endmodule
+
+
+
+
+
+
+
+
 
