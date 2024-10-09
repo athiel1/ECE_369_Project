65c95dba236066fe2620f110c1d96bc5b2d85bb4
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-09 15:40:18 -0700

    Update ID_EX.v

--- ID_EX.v
+++ ID_EX.v
@@ -1,12 +1,13 @@
 `timescale 1ns / 1ps
 
 
-module IF_ID(PC_in, readReg1, readReg2, signExtend, instr_rt_in, instr_rd_in, control_WB_in, control_M_in, control_EX_in, PC_out); 
+module IF_ID(PC_in, readReg1_in, readReg2_in, signExtend_in, instr_rt_in, instr_rd_in, control_WB_in, control_M_in, control_EX_in, 
+             PC_out, readReg1_out, readReg2_out, signExtend_out, instr_rt_out, instr_rd_out, control_WB_out, control_M_out, control_EX_out); 
   
   input [31:0] PC_in;
-  input [4:0] readReg1;
-  input [4:0] readReg2;
-  input [31:0] signExtend;
+  input [4:0] readReg1_in;
+  input [4:0] readReg2_in;
+  input [31:0] signExtend_in;
   input [4:0] instr_rt_in;
   input [4:0] instr_rd_in;
   input [31:0] control_WB_in;
@@ -17,7 +18,14 @@ module IF_ID(PC_in, readReg1, readReg2, signExtend, instr_rt_in, instr_rd_in, co
   
 
   output [31:0] PC_out;
-  output [31:0] instructionMem_out;
+  output [4:0] readReg1_out;
+  output [4:0] readReg2_out;
+  output [31:0] signExtend_out;
+  output [4:0] instr_rt_out;
+  output [4:0] instr_rd_out;
+  output [31:0] control_WB_out;
+  output [31:0] control_M_out;
+  output [31:0] control_EX_out;
 
   always @(*) begin
     
