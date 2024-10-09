b791303f5963dd2eb489f1a627c10bf960d8ca0f
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-09 15:53:30 -0700

    Update EX_MEM.v

--- EX_MEM.v
+++ EX_MEM.v
@@ -1,6 +1,7 @@
 `timescale 1ns / 1ps
 
-module EX_MEM(PC_in, instructionMem_in, PC_out, instructionMem_out); 
+module EX_MEM(WB_in, M_in, addResult_in, ALUZero_in, ALUResult_in, readReg2_in, muxResult_in,
+              WB_out, Branch, MemRead, MemWrite, addResult_out, ALUZero_out, ALUResult_out, readReg2_out, muxResult_out); 
   
   input [31:0] WB_in;
   input [31:0] M_in
