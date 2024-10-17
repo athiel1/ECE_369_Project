8eac205a5b2ab6973473b4f5cc5fd821f0214fc5
meganzupancic <118412810+meganzupancic@users.noreply.github.com>
2024-10-16 18:12:10 -0700

    Update Controller.v

--- Controller.v
+++ Controller.v
@@ -78,99 +78,198 @@ module Controller(Clk, Rst, Instruction, Zero, RegDst, ALUOp, ALUZero, ALUSrc, B
                 6'b000000: begin // R-Type
                     RegDst <= 1;
                     ALUOp <= 2'b00;
-                    ALUZero <= 0;     //doesn't matter
+                    //ALUZero <= 0;     //doesn't matter
                     ALUSrc <= 0;
                     Branch <= 0;      //doesn't matter
                     MemRead <= 0;     //doesn't matter
                     MemWrite <= 0;
                     MemtoReg <= 1;
                     RegWrite <= 1;
-                    PCSrc <= 0;       //doesn't matter
+                    //PCSrc <= 0;       //doesn't matter
                 end
                 6'b001000: begin // ADDI
                     RegDst <= 0;      // result goes to register rt [20:16]
                     ALUOp <= 2'b01;
-                    ALUZero <= 0;     //doesn't matter
+                    //ALUZero <= 0;     //doesn't matter
                     ALUSrc <= 1;      // takes the 'imm' value at the mux
                     Branch <= 0;      //doesn't matter
                     MemRead <= 0;     //doesn't matter
                     MemWrite <= 0;
                     MemtoReg <= 1;
                     RegWrite <= 1;
-                    PCSrc <= 0;       //doesn't matter
+                    //PCSrc <= 0;       //doesn't matter
                 end
                 6'b001100: begin // ANDI
                     RegDst <= 0;      // result goes to register rt [20:16]
                     ALUOp <= 2'b01;
-                    ALUZero <= 0;     //doesn't matter
+                    //ALUZero <= 0;     //doesn't matter
                     ALUSrc <= 1;      // takes the 'imm' value at the mux
                     Branch <= 0;      //doesn't matter
                     MemRead <= 0;     //doesn't matter
                     MemWrite <= 0;
                     MemtoReg <= 1;
                     RegWrite <= 1;
-                    PCSrc <= 0;       //doesn't matter
+                    //PCSrc <= 0;       //doesn't matter
                 end
                 6'b001101: begin // ORI
                     RegDst <= 0;      // result goes to register rt [20:16]
                     ALUOp <= 2'b01;
-                    ALUZero <= 0;     //doesn't matter
+                    //ALUZero <= 0;     //doesn't matter
                     ALUSrc <= 1;      // takes the 'imm' value at the mux
                     Branch <= 0;      //doesn't matter
                     MemRead <= 0;     //doesn't matter
                     MemWrite <= 0;
                     MemtoReg <= 1;
                     RegWrite <= 1;
-                    PCSrc <= 0;       //doesn't matter
+                    //PCSrc <= 0;       //doesn't matter
                 end
                 6'b001110: begin // XORI
                     RegDst <= 0;      // result goes to register rt [20:16]
                     ALUOp <= 2'b01;
-                    ALUZero <= 0;     //doesn't matter
+                    //ALUZero <= 0;     //doesn't matter
                     ALUSrc <= 1;      // takes the 'imm' value at the mux
                     Branch <= 0;      //doesn't matter
                     MemRead <= 0;     //doesn't matter
                     MemWrite <= 0;
                     MemtoReg <= 1;
                     RegWrite <= 1;
-                    PCSrc <= 0;       //doesn't matter
+                    //PCSrc <= 0;       //doesn't matter
                 end
                 6'b100011: begin // Load
                     RegDst <= 0;
                     ALUOp <= 2'b01;
-                    ALUZero <= 0;     //doesn't matter
+                    //ALUZero <= 0;     //doesn't matter
                     ALUSrc <= 1;
                     Branch <= 0;      //doesn't matter
                     MemRead <= 1;
                     MemWrite <= 0;
                     MemtoReg <= 1;
                     RegWrite <= 1;
-                    PCSrc <= 0;       //doesn't matter
+                    //PCSrc <= 0;       //doesn't matter
                 end
                 6'b101011: begin // Store
                     RegDst <= 0;
                     ALUOp <= 2'b01;
-                    ALUZero <= 0;     //doesn't matter
+                    //ALUZero <= 0;     //doesn't matter
                     ALUSrc <= 1;
                     Branch <= 0;      //doesn't matter
                     MemRead <= 0;
                     MemWrite <= 1;
                     MemtoReg <= 0;    //doesn't matter
                     RegWrite <= 0;
-                    PCSrc <= 0;       //doesn't matter
+                    //PCSrc <= 0;       //doesn't matter
                 end
-                6'b000100: begin // Branch
+                6'b101000: begin // Store Byte
                     RegDst <= 0;
-                    ALUOp <= 2'b10;   // doesn't matter?
-                    ALUZero <= 1;     
-                    ALUSrc <= 0;
+                    ALUOp <= 2'b01;
+                    //ALUZero <= 0;     //doesn't matter
+                    ALUSrc <= 1;
+                    Branch <= 0;      //doesn't matter
+                    MemRead <= 0;
+                    MemWrite <= 1;
+                    MemtoReg <= 0;    //doesn't matter
+                    RegWrite <= 0;
+                    //PCSrc <= 0;       //doesn't matter
+                end
+                6'b100000: begin // Load Byte
+                    RegDst <= 0;
+                    ALUOp <= 2'b01;
+                    //ALUZero <= 0;     //doesn't matter
+                    ALUSrc <= 1;
+                    Branch <= 0;      //doesn't matter
+                    MemRead <= 0;
+                    MemWrite <= 1;
+                    MemtoReg <= 0;    //doesn't matter
+                    RegWrite <= 0;
+                    //PCSrc <= 0;       //doesn't matter
+                end
+                6'b101001: begin // Store half
+                    RegDst <= 0;
+                    ALUOp <= 2'b01;
+                    //ALUZero <= 0;     //doesn't matter
+                    ALUSrc <= 1;
+                    Branch <= 0;      //doesn't matter
+                    MemRead <= 0;
+                    MemWrite <= 1;
+                    MemtoReg <= 0;    //doesn't matter
+                    RegWrite <= 0;
+                    //PCSrc <= 0;       //doesn't matter
+                end
+                6'b100001: begin // Load half
+                    RegDst <= 0;
+                    ALUOp <= 2'b01;
+                    //ALUZero <= 0;     //doesn't matter
+                    ALUSrc <= 1;
+                    Branch <= 0;      //doesn't matter
+                    MemRead <= 0;
+                    MemWrite <= 1;
+                    MemtoReg <= 0;    //doesn't matter
+                    RegWrite <= 0;
+                    //PCSrc <= 0;       //doesn't matter
+                end
+                6'b000001: begin // BGEZ & BLTZ
+                    RegDst <= 0;
+                    ALUOp <= 2'b10;
+                    //ALUZero <= 1;     
+                    ALUSrc <= 2;      //want to compare to 0
                     Branch <= 1;     
                     MemRead <= 0;     //doesn't matter
                     MemWrite <= 0;    //has to be 0 so we don't overwrite
                     MemtoReg <= 0;    //doesn't matter
                     RegWrite <= 0;    //has to be zero so we don't overwrite
-                    PCSrc <= Branch & Zero;       
+                    //PCSrc <= Branch & Zero;       
                 end
+                6'b000100: begin // BEQ
+                    RegDst <= 0;
+                    ALUOp <= 2'b10;
+                    //ALUZero <= 1;     
+                    ALUSrc <= 0;      
+                    Branch <= 1;     
+                    MemRead <= 0;     //doesn't matter
+                    MemWrite <= 0;    //has to be 0 so we don't overwrite
+                    MemtoReg <= 0;    //doesn't matter
+                    RegWrite <= 0;    //has to be zero so we don't overwrite
+                    //PCSrc <= Branch & Zero;       
+                end
+                6'b000101: begin // BNE
+                    RegDst <= 0;
+                    ALUOp <= 2'b10;
+                    //ALUZero <= 1;     
+                    ALUSrc <= 0;      
+                    Branch <= 1;     
+                    MemRead <= 0;     //doesn't matter
+                    MemWrite <= 0;    //has to be 0 so we don't overwrite
+                    MemtoReg <= 0;    //doesn't matter
+                    RegWrite <= 0;    //has to be zero so we don't overwrite
+                    //PCSrc <= Branch & Zero;       
+                end
+                6'b000111: begin // BGTZ
+                    RegDst <= 0;
+                    ALUOp <= 2'b10;
+                    //ALUZero <= 1;     
+                    ALUSrc <= 2;      //want to compare to 0     
+                    Branch <= 1;     
+                    MemRead <= 0;     //doesn't matter
+                    MemWrite <= 0;    //has to be 0 so we don't overwrite
+                    MemtoReg <= 0;    //doesn't matter
+                    RegWrite <= 0;    //has to be zero so we don't overwrite
+                    //PCSrc <= Branch & Zero;       
+                end
+                6'b000110: begin // BLEZ
+                    RegDst <= 0;
+                    ALUOp <= 2'b10;
+                    //ALUZero <= 1;     
+                    ALUSrc <= 2;      //want to compare to 0      
+                    Branch <= 1;     
+                    MemRead <= 0;     //doesn't matter
+                    MemWrite <= 0;    //has to be 0 so we don't overwrite
+                    MemtoReg <= 0;    //doesn't matter
+                    RegWrite <= 0;    //has to be zero so we don't overwrite
+                    //PCSrc <= Branch & Zero;       
+                end
+                
+                //FIXME: add instructions for j, jr, jal
+                
                 default: begin
                     Debug = 1;
                 end
