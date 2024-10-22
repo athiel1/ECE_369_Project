6990f80e1bf525ca99c2d628095a21e9b8ca2acb
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-22 16:15:24 -0700

    Update MEM_WB.v

--- MEM_WB.v
+++ MEM_WB.v
@@ -1,21 +1,26 @@
 `timescale 1ns / 1ps
 
-module MEM_WB(WB_in_MEMWB, readMem_in_MEMWB, ALUResult_in_MEMWB, muxResult_in_MEMWB, RegWrite_MEMWB, MemtoRead_MEMWB, readMem_out_MEMWB, ALUResult_out_MEMWB, muxResult_out_MEMWB);
-  
-  input [31:0] readMem_in_MEMWB;
-  input [31:0] ALUResult_in_MEMWB;
-  input [31:0] muxResult_in_MEMWB;
+module MEM_WB(MemtoReg_in_MEMWB, RegWrite_in_MEMWB, ALUResult_in_MEMWB, ReadData2_in_MEMWB, mux2_result_in_MEMWB, 
+              MemtoReg_out_MEMWB, RegWrite_out_MEMWB, ALUResult_out_MEMWB, ReadData2_out_MEMWB, mux2_result_out_MEMWB);
 
-  output RegWrite_MEMWB;
-  output MemtoRead_MEMWB;
-  output [31:0] readMem_out_MEMWB;
-  output [31:0] ALUResult_out_MEMWB;
-  output [31:0] muxResult_out_MEMWB;
+  input MemtoReg_in_MEMWB;
+  input RegWrite_in_MEMWB;
+  input [31:0]ALUResult_in_MEMWB;
+  input [4:0] ReadData2_in_MEMWB;
+  input [4:0] mux2_result_in_MEMWB;
+
+  output MemtoReg_out_MEMWB;
+  output RegWrite_out_MEMWB;
+  output [31:0]ALUResult_out_MEMWB;
+  output [4:0] ReadData2_out_MEMWB;
+  output [4:0] mux2_result_out_MEMWB;
 
   always @(*) begin
-    readMem_out_MEMWB = readMem_in_MEMWB;
+    MemtoReg_out_MEMWB = MemtoReg_in_MEMWB;
+    RegWrite_out_MEMWB = RegWrite_in_MEMWB;
     ALUResult_out_MEMWB = ALUResult_in_MEMWB;
-    muxResult_out_MEMWB = muxResult_in_MEMWB;
+    ReadData2_out_MEMWB = ReadData2_in_MEMWB;
+    mux2_result_out_MEMWB = mux2_result_in_MEMWB;
   end
 
 
