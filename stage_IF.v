34163731e68ffffcf4a2bda424ce82624001948b
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-21 17:08:58 -0700

    Update stage_IF.v

--- stage_IF.v
+++ stage_IF.v
@@ -1,5 +1,48 @@
-module stage_IF ();
-    input 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/06/2024 09:12:54 AM
+// Design Name: 
+// Module Name: top_level_lab1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
 
+module stage_IF(Clk, Reset, out7, en_out);
+
+    input Reset, Clk;
+    output [6:0] out7;
+    output [7:0] en_out;
+    
+    wire [31:0] PCResult;
+    wire ClkOut;
+    wire [31:0] Instruction;
+    
+    //wire [31:0] PCResult; //do we need to declare this again?
+    
+    //ClkDiv(Clk, Rst, ClkOut);
+    ClkDiv b1(Clk, 0, ClkOut);
+    
+    //InstructionFetchUnit(Instruction, PCResult, Reset, Clk);
+    InstructionFetchUnit b2(Instruction, PCResult, Reset, ClkOut);
+    
+    //Two4DigitDisplay(Clk, NumberA, NumberB, out7, en_out);
+    Two4DigitDisplay b3(Clk, Instruction[15:0], PCResult[15:0], out7, en_out);
+    
 endmodule
+
+
+
+
