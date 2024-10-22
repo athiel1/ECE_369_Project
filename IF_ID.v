3c70d18673071d7bf749a4054f201b5c482dca9f
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-22 15:47:15 -0700

    Update IF_ID.v

--- IF_ID.v
+++ IF_ID.v
@@ -1,17 +1,17 @@
 `timescale 1ns / 1ps
 
 
-module IF_ID(PC_in_IFID, instructionMem_in_IFID, PC_out_IFID, instructionMem_out_IFID); 
+module IF_ID(PCAdder_in_IFID, Instruction_in_IFID, PCAdder_out_IFID, Instruction_out_IFID); 
   
-  input [31:0] PC_in_IFID;
-  input [31:0] instructionMem_in_IFID;
+  input [31:0] PCAdder_in_IFID;
+  input [31:0] Instruction_in_IFID;
 
-  output [31:0] PC_out_IFID;
-  output [31:0] instructionMem_out_IFID;
+  output [31:0] PCAdder_out_IFID;
+  output [31:0] Instruction_out_IFID;
 
   always @(*) begin
-    PC_out_IFID <= PC_in_IFID;
-    instructionMem_out_IFID <= instructionMem_in_IFID;
+    PCAdder_out_IFID <= PCAdder_in_IFID;
+    Instruction_out_IFID <= Instruction_in_IFID;
   end
 
 
