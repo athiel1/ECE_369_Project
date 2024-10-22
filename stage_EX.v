6a8ce5adbcbb331424691f1430465711ec4c3ef6
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-22 15:35:23 -0700

    Update stage_EX.v

--- stage_EX.v
+++ stage_EX.v
@@ -1,60 +1,60 @@
-module stage_EX (RegWrite_in, MemtoReg_in, Branch_in, MemRead_in, MemWrite_in, RegDst, ALUOp, ALUSrc, PCAddResult, ReadData1, ReadData2_in, SignExtResult, rt, rd, 
-                 RegWrite_out, MemtoReg_out, Branch_out, MemRead_out, MemWrite_out, ALUAddResult, Zero, ALUResult, ReadData2_out, mux2_result);
+module stage_EX (RegWrite_in_EX, MemtoReg_in_EX, Branch_in_EX, MemRead_in_EX, MemWrite_in_EX, RegDst_EX, ALUOp_EX, ALUSrc_EX, PCAddResult_EX, ReadData1_EX, ReadData2_in_EX, SignExtResult_EX, rt_EX, rd_EX, 
+                 RegWrite_out_EX, MemtoReg_out_EX, Branch_out_EX, MemRead_out_EX, MemWrite_out_EX, ALUAddResult_EX, Zero_EX, ALUResult_EX, ReadData2_out_EX, mux2_result_EX);
 
-  input RegWrite_in;
-  input MemtoReg_in;
-  input Branch_in;
-  input MemRead_in;
-  input MemWrite_in;
-  input RegDst;
-  input [5:0] ALUOp;
-  input ALUSrc;
-  input [31:0] PCAddResult;
-  input [4:0] ReadData1;
-  input [4:0] ReadData2_in;
-  input [31:0] SignExtResult;
-  input [4:0] rt;
-  input [4:0] rd;
+  input RegWrite_in_EX;
+  input MemtoReg_in_EX;
+  input Branch_in_EX;
+  input MemRead_in_EX;
+  input MemWrite_in_EX;
+  input RegDst_EX;
+  input [5:0] ALUOp_EX;
+  input ALUSrc_EX;
+  input [31:0] PCAddResult_EX;
+  input [4:0] ReadData1_EX;
+  input [4:0] ReadData2_in_EX;
+  input [31:0] SignExtResult_EX;
+  input [4:0] rt_EX;
+  input [4:0] rd_EX;
 
-  output RegWrite_out;
-  output MemtoReg_out;
-  output Branch_out;
-  output MemRead_out;
-  output MemWrite_out;
-  output [31:0] ALUAddResult;
-  output Zero;
-  output [31:0] ALUResult;
-  output ReadData2_out;
+  output RegWrite_out_EX;
+  output MemtoReg_out_EX;
+  output Branch_out_EX;
+  output MemRead_out_EX;
+  output MemWrite_out_EX;
+  output [31:0] ALUAddResult_EX;
+  output Zero_EX;
+  output [31:0] ALUResult_EX;
+  output ReadData2_out_EX;
   output mux2_result;
 
-  wire SL_result;
-  wire mux1_result;
-  wire ALUControl;
+  wire SL_result_EX;
+  wire mux1_result_EX;
+  wire ALUControl_EX;
 
-  assign RegWrite_out = RegWrite_in;
-  assign MemtoReg_out = MemtoReg_in;
-  assign Branch_out = Branch_in;
-  assign MemRead_out = MemRead_in;
-  assign MemWrite_out = MemWrite_in;
-  assign ReadData2_out = ReadData2_in;
+  assign RegWrite_out_EX = RegWrite_in_EX;
+  assign MemtoReg_out_EX = MemtoReg_in_EX;
+  assign Branch_out_EX = Branch_in_EX;
+  assign MemRead_out_EX = MemRead_in_EX;
+  assign MemWrite_out_EX = MemWrite_in_EX;
+  assign ReadData2_out_EX = ReadData2_in_EX;
 
   //ShiftLeft2(in, out);
-  ShiftLeft2 c1(SignExtResult, SL_result);
+  ShiftLeft2 c1(SignExtResult_EX, SL_result_EX);
   
   //ALUAdd(A, B, ALUAddResult);
-  ALUAdd c2(PCAddResult, SL_result, ALUAddResult);
+  ALUAdd c2(PCAddResult_EX, SL_result_EX, ALUAddResult_EX);
 
   //Mux32Bit2To1(inA, inB, sel, out);
-  Mux32Bit2To1 c3(ReadData2_in, SignExtResult, ALUSrc, mux1_result);
+  Mux32Bit2To1 c3(ReadData2_in_EX, SignExtResult_EX, ALUSrc_EX, mux1_result_EX);
 
   //ALUControl(ALUOp, funct, ALUControl);
-  ALUControl c4(ALUOp, SignExtResult, ALUControl);    // if there is an error, we want 6 bits from SignExtResult
+  ALUControl c4(ALUOp_EX, SignExtResult_EX, ALUControl_EX);    // if there is an error, we want 6 bits from SignExtResult
 
   //ALU32Bit(ALUControl, A, B, ALUResult, Zero);
-  ALU32Bit c5(ALUControl, ReadData1, mux1_result, ALUResult, Zero);
+  ALU32Bit c5(ALUControl_EX, ReadData1_EX, mux1_result_EX, ALUResult_EX, Zero_EX);
 
   //Mux32Bit2To1(inA, inB, sel, out);
-  Mux32Bit2To1 c6(rt, rd, RegDst, mux2_result);
+  Mux32Bit2To1 c6(rt_EX, rd_EX, RegDst_EX, mux2_result_EX);
   
 
   
