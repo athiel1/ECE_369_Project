5cd311632fdb44f0d349e9fe7e8d3647ed7c6f3e
meganzupancic <118412810+meganzupancic@users.noreply.github.com>
2024-10-22 15:38:26 -0700

    Update MEM_WB.v

--- MEM_WB.v
+++ MEM_WB.v
@@ -1,22 +1,22 @@
 `timescale 1ns / 1ps
 
-module MEM_WB(WB_in, readMem_in, ALUResult_in, muxResult_in, RegWrite, MemtoRead, readMem_out, ALUResult_out, muxResult_out);
+module MEM_WB(WB_in_MEMWB, readMem_in_MEMWB, ALUResult_in_MEMWB, muxResult_in_MEMWB, RegWrite_MEMWB, MemtoRead_MEMWB, readMem_out_MEMWB, ALUResult_out_MEMWB, muxResult_out_MEMWB);
   
-  input [31:0] WB_in;
-  input [31:0] readMem_in;
-  input [31:0] ALUResult_in;
-  input [31:0] muxResult_in;
+  input [31:0] WB_in_MEMWB;
+  input [31:0] readMem_in_MEMWB;
+  input [31:0] ALUResult_in_MEMWB;
+  input [31:0] muxResult_in_MEMWB;
 
-  output RegWrite;
-  output MemtoRead;
-  output [31:0] readMem_out;
-  output [31:0] ALUResult_out;
-  output [31:0] muxResult_out;
+  output RegWrite_MEMWB;
+  output MemtoRead_MEMWB;
+  output [31:0] readMem_out_MEMWB;
+  output [31:0] ALUResult_out_MEMWB;
+  output [31:0] muxResult_out_MEMWB;
 
   always @(*) begin
-    readMem_out = readMem_in;
-    ALUResult_out = ALUResult_in;
-    muxResult_out = muxResult_in;
+    readMem_out_MEMWB = readMem_in_MEMWB;
+    ALUResult_out_MEMWB = ALUResult_in_MEMWB;
+    muxResult_out_MEMWB = muxResult_in_MEMWB;
   end
 
 
