74391047875c39e976fb7a3fa73cd13f6122625b
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-21 18:59:09 -0700

    Update stage_WB.v

--- stage_WB.v
+++ stage_WB.v
@@ -1,5 +1,15 @@
-module stage_WB ();
+module stage_WB (ReadData, ALUResult, MemtoReg, RegWrite, mux3_result, mux2_result);
 
+  input [31:0] ReadData;
+  input [31:0] ALUResult;
+  input MemtoReg;
 
+  output RegWrite;
+  output mux3_result;
+  output mux2_result;
+
+  //Mux32Bit2To1(inA, inB, sel, out);
+  Mux32Bit2To1 e1(ReadData, ALUResult, MemtoReg, mux3_result);
+  
 
 endmodule
