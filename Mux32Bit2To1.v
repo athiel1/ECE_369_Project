6315250b31f705a9442fb036ee280a2cdf69b64b
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-21 18:31:11 -0700

    Update Mux32Bit2To1.v

--- Mux32Bit2To1.v
+++ Mux32Bit2To1.v
@@ -7,13 +7,14 @@
 // Description - Performs signal multiplexing between 2 32-Bit words.
 ////////////////////////////////////////////////////////////////////////////////
 
-module Mux32Bit2To1(out, inA, inB, sel);
-
-    output reg [31:0] out;
+module Mux32Bit2To1(inA, inB, sel, out);
     
     input [31:0] inA;
     input [31:0] inB;
     input sel;
+
+    output reg [31:0] out;
+    
     // assign out = sel? inB: inA;   for this line, no reg in line 12
     always @(*) begin
 
