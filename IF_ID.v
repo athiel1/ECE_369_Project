296f38a9f74009e34c448e5f715b2bdf57d01de8
meganzupancic <118412810+meganzupancic@users.noreply.github.com>
2024-10-22 16:10:32 -0700

    Update IF_ID.v

--- IF_ID.v
+++ IF_ID.v
@@ -6,8 +6,8 @@ module IF_ID(PCAdder_in_IFID, Instruction_in_IFID, PCAdder_out_IFID, Instruction
   input [31:0] PCAdder_in_IFID;
   input [31:0] Instruction_in_IFID;
 
-  output [31:0] PCAdder_out_IFID;
-  output [31:0] Instruction_out_IFID;
+  output reg [31:0] PCAdder_out_IFID;
+  output reg [31:0] Instruction_out_IFID;
 
   always @(*) begin
     PCAdder_out_IFID <= PCAdder_in_IFID;
