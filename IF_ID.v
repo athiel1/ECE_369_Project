c1c32c6bd6ca5bb5c460bc5b4508d9e2a8702f2f
meganzupancic <118412810+meganzupancic@users.noreply.github.com>
2024-10-25 20:53:49 -0700

    Update IF_ID.v

--- IF_ID.v
+++ IF_ID.v
@@ -1,19 +1,21 @@
 `timescale 1ns / 1ps
 
 
-module IF_ID(PCAdder_in_IFID, Instruction_in_IFID, PCAdder_out_IFID, Instruction_out_IFID); 
+module IF_ID(PCAdder_in_IFID, Instruction_in_IFID, PCAdder_out_IFID, Instruction_out_IFID
+            Clk_in_IFID, Clk_out_IFID); 
   
   input [31:0] PCAdder_in_IFID;
   input [31:0] Instruction_in_IFID;
-  //input Clk_in_IFID, Clk_out_IFID;
+  input Clk_in_IFID;
 
   output reg [31:0] PCAdder_out_IFID;
   output reg [31:0] Instruction_out_IFID;
-  //output reg Clk_in_IFID;
+  output reg Clk_in_IFID;
 
   always @(posedge Clk) begin
     PCAdder_out_IFID <= PCAdder_in_IFID;
     Instruction_out_IFID <= Instruction_in_IFID;
+    Clk_in_IFID <= Clk_out_IFID;
   end
 
 
